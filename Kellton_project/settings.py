5b2e1fea858d204595f97bd3f70c3a79dedac3dd
Pan Panda <mabraq@gmail.com>
2024-07-16 16:18:37 +0200

    Reformated settings and urls

--- Kellton_project/settings.py
+++ Kellton_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'family_budget.apps.FamilyBudgetConfig',
+    "family_budget.apps.FamilyBudgetConfig",
 ]
 
 MIDDLEWARE = [
@@ -55,8 +55,7 @@ ROOT_URLCONF = "Kellton_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
